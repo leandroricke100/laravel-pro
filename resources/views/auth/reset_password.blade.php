9c5bf22dfe4493437a335c3145aa08cb30651aef
Leandro Henrique <leandrohenriquesilvalima@gmail.com>
2025-03-01 11:33:03 -0700

    links register and login

--- resources/views/auth/reset_password.blade.php
+++ resources/views/auth/reset_password.blade.php
@@ -1,1 +1,54 @@
-reseet password
+@extends('layouts.auth')
+
+@section('body-class', 'register-page')
+
+@section('content')
+    <div class="register-box">
+        <div class="register-logo">
+            <a href="{{ route('login') }}"><b>Admin</b>LTE</a>
+        </div>
+        <!-- /.register-logo -->
+        <div class="card">
+            <div class="card-body register-card-body">
+                <p class="register-box-msg">Reset Password</p>
+
+
+                <form action="{{ route('password.update') }}" method="post">
+                    @csrf
+                    <input type="hidden" name="token" value="{{ request()->token }}"/>
+                    <div class="input-group mb-3">
+                        <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+                        <input readonly type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ request()->email }}"/>
+                        @error('email')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" value="{{ old('password') }}"/>
+                        @error('password')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                        <input type="password" name="password_confirmation" class="form-control" placeholder="Password Confirmation" />
+                    </div>
+                    <!--begin::Row-->
+                    <div class="row">
+
+                        <div class="d-grid gap-2">
+                            <button type="submit" class="btn btn-primary">Reset Password</button>
+                        </div>
+
+                        <!-- /.col -->
+                    </div>
+                    <!--end::Row-->
+                </form>
+
+            </div>
+            <!-- /.register-card-body -->
+        </div>
+    </div>
+@endsection
--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -142,14 +142,14 @@
           class="user-image rounded-circle shadow"
           alt="User Image"
         /> --}}
-                    <span class="d-none d-md-inline">Alexander Pierce</span>
+                    <span class="d-none d-md-inline">{{ auth()->user()->name ?? 'N/D' }}</span>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
                     <!--begin::User Image-->
                     <li class="user-header text-bg-primary">
                         <img src="{{ Vite::asset('resources/img/user2-160x160.jpg ') }}" class="rounded-circle shadow" alt="User Image" />
                         <p>
-                            Alexander Pierce - Web Developer
+                            {{ auth()->user()->name ?? 'N/D'}}
                             <small>Member since Nov. 2023</small>
                         </p>
                     </li>
